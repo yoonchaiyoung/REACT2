e2e0c1ef46758927fcd696855c0f8ef735b4c1a1
yoonchaiyoung <domo62@naver.com>
2020-11-30 09:58:12 +0900

    initial commit

---
+++ react 환경설정.md
@@ -0,0 +1,17 @@
+* cmd(관리자 모드)
+
+D:
+
+cd data-science
+
+cd REACT
+
+cd (현재 위치 D:/data-science/REACT인지 확인)
+
+npx create-react-app react-chap02
+
+* visual studio code terminal
+
+cd react-chap02
+
+npm start
---
+++ src/UseStateComponent.js
@@ -0,0 +1,12 @@
+import React, { useState } from "react";
+
+// useState 및 useffect에 대한 설명
+
+const UseStateComponent = () => {
+  const [name, setName] = useState("");
+  const [nickname, setNickname] = useState("");
+
+  return <div></div>;
+};
+
+export default UseStateComponent;
